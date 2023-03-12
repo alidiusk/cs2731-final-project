20959d49ee8e2160737a2c37e1264a3f3deb2d59
sim1029 <sps67@pitt.edu>
2023-03-12 19:56:24 -0400

    add iteration parameter to script and generate 2100 questions

--- data/gen.py
+++ data/gen.py
@@ -40,9 +40,7 @@ def verify(output):
         
         # Replace pipes with commas and commas with pipes, triggering of the error below
         # is indicitive of an extra pipe operator somewhere in the output. 
-        print("\nOUTPUT\n", StringIO(output.strip()).read())
         trans_output = ''.join([',' if char == '|' else '|' if char == ',' else char for char in output.strip()])
-        print("\nTRANS_OUTPUT\n", StringIO(trans_output.strip()).read())
         return pd.read_csv(StringIO(trans_output), sep=",", header=None, names=COLUMNS)
     except Exception as err:
         print("-" * 80)
@@ -66,41 +64,57 @@ def verify(output):
 
 # Specify number of questions to generate as first command line argument
 # Ex. $ python3 gen.py fifty
+# Specify number of iterations in second parameter
+# Ex. $ python3 gen.py fifty 10
 def main():
-    output = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-          #{"role": "system", "content": "You are a helpful assistant that <generates X>."},
-          {"role": "user", "content": \
-                    "Provide {} multiple choice science questions, in CSV format \
-                      replacing the comma separator \",\" with a pipe \"|\" separator \
-                        and with newlines at the end of each row, formatted into columns \
-                          Topic|Question|Answer|Distractor1|Distractor|Distractor3".format(sys.argv[1])}
-      ]
-    )
+    successful = 0
+    review = 0
+    total_cost = 0
+    for i in range(int(sys.argv[2])):
+      output = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            #{"role": "system", "content": "You are a helpful assistant that <generates X>."},
+            {"role": "user", "content": \
+                      "Provide {} multiple choice science questions, in CSV format \
+                        replacing the comma separator \",\" with a pipe \"|\" separator \
+                          and with newlines at the end of each row, formatted into columns \
+                            Topic|Question|Answer|Distractor1|Distractor|Distractor3".format(sys.argv[1])}
+        ]
+      )
 
-    text_output = output['choices'][0]['message']['content']
-    total_token_cost = output['usage']['total_tokens']
+      text_output = output['choices'][0]['message']['content']
+      total_token_cost = output['usage']['total_tokens']
+      total_cost += int(total_token_cost)
 
-    print(f'Text output:\n{text_output}')
-    print(f'Total response token cost: {total_token_cost}')
+      print(f'Response {i+1} Token Cost: {total_token_cost}')
 
-    df = verify(text_output)
-    file_id = uuid.uuid4()
+      df = verify(text_output)
+      file_id = uuid.uuid4()
 
-    if df is not None:
-        # TODO: better directory management; this should be ran in "data" directory
-        filename = f"{file_id}.csv"
-        print(f"Saving to {filename}")
-        df.to_csv(filename, index=False)
-    else:
-        # Save file for review
-        filename = f"REVIEW-{file_id}.csv"
-        print(f"Saving file for review in {filename}.")
+      if df is not None:
+          # TODO: better directory management; this should be ran in "data" directory
+          filename = f"{file_id}.csv"
+          print(f"Saving to {filename}")
+          df.to_csv(filename, index=False)
+          successful += 1
+      else:
+          # Save file for review
+          print(f'Text for review:\n{text_output}')
+          filename = f"REVIEW-{file_id}.csv"
+          print(f"Saving file for review in {filename}.")
 
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(text_output)
+          with open(filename, "w", encoding="utf-8") as file:
+              file.write(text_output)
+          review += 1
 
+    print("-" * 80)
+    print("RESULTS SUMMARY\n", sys.argv[1], " x ", sys.argv[2], " questions generated\n", sep="", end="")
+    print("SUCCESSFUL FILES:", successful)
+    print("FILES TO REVIEW:", review)
+    print("TOTAL_TOKEN_COST:", total_cost)
+    print("Cost in USD:", (total_cost / 1000) * 0.002)
+    print("-" * 80)
 
 if __name__ == '__main__':
     main()
